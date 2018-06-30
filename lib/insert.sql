a3c9f778c86b39d01661b5d0ccfd43daa99ac489
thuyanduong-flatiron <thuyan.duong@flatironschool.com>
2018-06-29 20:27:24 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, 'Mr. Chocolate', 6, 'M', 'Brown', 'mean', 1);
+INSERT INTO bears VALUES (2, 'Rowdy', 4, 'M', 'Polar', 'nice', 1);
+INSERT INTO bears VALUES (3, 'Tabitha', 9, 'M', 'Black', 'nice', 1);
+INSERT INTO bears VALUES (4, 'Sergeant Brown', 2, 'M', 'Panda', 'nice', 1);
+INSERT INTO bears VALUES (5, 'Melissa', 8, 'M', 'Red', 'nice', 1);
+INSERT INTO bears VALUES (6, 'Grinch', 3, 'M', 'Purple', 'nice', 1);
+INSERT INTO bears VALUES (7, 'Wendy', 5, 'M', 'Brown', 'nice', 1);
+INSERT INTO bears VALUES (8, NULL, 8, 'M', 'Black', 'mean', 1);
