5a68a11c8fa4bd4742efd73847b85fa9cbd5485a
finale100 <finale150@gmail.com>
2018-07-03 16:29:03 -0400

    done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+Insert into bears (name, age, gender, color, temperament, alive) values ("Mr. Chocolate", 80, "M", "Black", "Sad", "Yes");
+Insert into bears (name, age, gender, color, temperament, alive) values ("Rowdy", 14, "F", "Brown", "Happy", "Yes");
+Insert into bears (name, age, gender, color, temperament, alive) values ("Tabitha", 44, "F", "Polar", "Chill", "No");
+Insert into bears (name, age, gender, color, temperament, alive) values ("Sergeant Brown", 28, "M", "Panda", "Angry", "Yes");
+Insert into bears (name, age, gender, color, temperament, alive) values ("Melissa", 28, "F", "Pooh", "Hungry", "Yes");
+Insert into bears (name, age, gender, color, temperament, alive) values ("Grinch", 75, "M", "Black", "Playful", "No");
+Insert into bears (name, age, gender, color, temperament, alive) values ("Wendy", 12, "F", "Brown", "Joyful", "Yes");
+Insert into bears (name, age, gender, color, temperament, alive) values (NULL, 23, "M", NULL, "Timid", "Yes");
